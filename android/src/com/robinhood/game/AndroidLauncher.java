313978e1321de16fa4975bc02cc2300aea01ab9c
adrianht <adrianhthompson@gmail.com>
2020-03-02 15:52:49 +0100

    init

---
+++ android/src/com/robinhood/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.robinhood.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.robinhood.game.RobinHood;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RobinHood(), config);
+	}
+}
---
+++ desktop/src/com/robinhood/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.robinhood.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.robinhood.game.RobinHood;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RobinHood(), config);
+	}
+}
