3ae4e86f53f00f15b6ee4c5032b191ba4865dc0a
unknown <eivind.rydningen@gmail.com>
2020-03-09 17:48:07 +0100

    Implement draft of base View and MainMenu

--- core/src/view/MenuView.java
+++ core/src/view/MenuView.java
@@ -2,18 +2,33 @@ package view;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 
 import controller.Controller;
 import model.Model;
 
 public class MenuView extends View {
+    private Button StartGameButton;
+    private Button SettingsButton;
+    private Button ExitButton;
 
-    public MenuView(Model model, Controller controller) {
-        super.setData(model.getData("state2"));
-        super.setController(controller);
+    public MenuView(Controller controller, Model model) {
+        //super.setData(model.getData("state2"));
+        //super.setController(controller);
+        super(controller, model);
 
         //TODO: render data from model for the given view
         super.batch = new SpriteBatch();
         super.img = new Texture("badlogic.jpg");
+
+        StartGameButton = new Button();
+        SettingsButton = new Button();
+        ExitButton = new Button();
     }
+
+    @Override
+    public void init(){
+        
+    }
+
 }
--- core/src/view/View.java
+++ core/src/view/View.java
@@ -1,28 +1,62 @@
 package view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import controller.Controller;
+import model.Model;
 
-public abstract class View {
+public abstract class View implements Screen {
     SpriteBatch batch;
     Texture img;
 
-    private String data;
-    private Controller controller;
+    //protected String data;
+    protected Controller controller;
+    protected Model world;
+    protected Stage UI;
+
+    public View (Controller controller, Model world){
+        this.controller = controller;
+        this.world = world;
+        UI = new Stage(new ScreenViewport());
+    }
+
+    // Lifecycle methods
+    @Override
+    final public void show() {
+
+        // Maps the controller
+        InputMultiplexer input = new InputMultiplexer();
+        input.addProcessor(UI);
+        Gdx.input.setInputProcessor(input);
+
+        // Screen specific
+        init();
+    }
+
+    public void init(){}
 
     public void render () {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(UI != null){
+            UI.act(delta);
+            UI.draw();
+        }
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
     }
 
     public void dispose () {
+        if(UI != null) UI.dispose();
+        UI = null;
         batch.dispose();
         img.dispose();
     }
