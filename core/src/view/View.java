30e25045911d5816b3a7a6e66315704fea8691ed
Nina Andal Aarvik <nina.andal.aarvik@gmail.com>
2020-03-30 10:32:45 +0200

    Added hexadecimal for colors

--- core/src/view/View.java
+++ core/src/view/View.java
@@ -9,9 +9,27 @@ public abstract class View extends Stage {
     protected Stage stage;
 
     public void render() {
-        Gdx.gl.glClearColor(130/255f, 230/255f, 250/255f, 255/255f);
+        float[] values = hextoRGB("#5f8db0");
+        Gdx.gl.glClearColor(values[0], values[1], values[2], 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
+    }
+
+    /* re-map RGB colors so they can be used in OpenGL */
+    private float[] map(float[]rgb) {
+        float[] result = new float[3];
+        result[0] = rgb[0] / 255;
+        result[1] = rgb[1] / 255;
+        result[2] = rgb[2] / 255;
+        return result;
+    }
+    //Can use hexadecimals for colors instead of the gl colors
+    public float[] hextoRGB(String hex) {
+        float[] rgbcolor = new float[3];
+        rgbcolor[0] = Integer.valueOf(hex.substring(1,3 ),16);
+        rgbcolor[1] = Integer.valueOf(hex.substring(3,5 ),16);
+        rgbcolor[2] = Integer.valueOf(hex.substring(5,7 ),16);
+        return map(rgbcolor);
     }
 
     @Override
