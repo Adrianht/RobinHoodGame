70011c41de0c7f71560d563f0fd9eae2b8da4f04
Ola Hermann Opheim <olahermannopheim@Olas-MacBook-Pro.local>
2020-04-14 11:10:40 +0200

    todos

--- android/src/com/robinhood/game/controller/FBConnector.java
+++ android/src/com/robinhood/game/controller/FBConnector.java
@@ -1,5 +1,6 @@
 package com.robinhood.game.controller;
 
+import com.badlogic.gdx.math.Vector2;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -8,6 +9,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class FBConnector {
+
+    // TODO: legge til unik game room refereanse delt mellom to spillere
 
     private DatabaseReference mDatabase;
 
@@ -19,8 +22,36 @@ public class FBConnector {
         // TODO: telle antall spillere etter endring
 
         // TODO: dersom det er to spillere i "availablePlayer" -> kall
-        // controller.initateGame()
+        // controller.initateGame(username1,username2);
 
+    }
+
+
+    public void createGameRoom() {
+
+        // TODO: lage default field move, og tilhorende listener onChange()
+        //  onChange skal kalle controller.registerMove()
+
+
+        // TODO: lage default field activeArrow, og tilhorende listener onChange()
+        //  onChange skal kalle controller.registerBuy()
+
+        // TODO: lage default field drawBow, og tilhorende listener onChange()
+        //  onChange skal kalle controller.registerDraw()
+
+    }
+
+    // TODO: dobbeltsjekk at disse metodene fungerer sammen med lytterne definert i createGameRoom,
+    //  slik at controller.register...-metodene blir kalt.
+    public static void setMove(boolean left) {
+        // TODO: push boolean-verdi "left" til move i gameroom
+    }
+    public static void setBuy(String type) {
+        // TODO: push string-verdi "type" til activeArrow i gameroom
+    }
+    public static void setDraw(Vector2 vector2) {
+        // TODO: check if Firebase can handle Vector2-objects
+        // TODO: push Vector2-verdi "vector2" til drawBow i gameroom
     }
 
 }
