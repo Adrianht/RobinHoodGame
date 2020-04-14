9ded7e4f8bda346e1533d37767640c273dc09ee2
adrianht <adrianhthompson@gmail.com>
2020-04-14 10:56:11 +0200

    working two player

--- android/src/com/robinhood/game/controller/FBConnector.java
+++ android/src/com/robinhood/game/controller/FBConnector.java
@@ -1,6 +1,7 @@
 package com.robinhood.game.controller;
 
-import com.google.firebase.database.ChildEventListener;
+import androidx.annotation.NonNull;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,12 +16,35 @@ public class FBConnector {
         mDatabase = FirebaseDatabase.getInstance().getReference().child("availablePlayer");
 
         // TODO: push denne spilleren med username
+        mDatabase.push().setValue(username);
+        System.out.println("player pushed");
+
 
         // TODO: telle antall spillere etter endring
+        //mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
 
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            int counter = 0;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    System.out.println(snapshot);
+                    counter += 1;
+                    if(counter == 2){
+                        System.out.println("INIT GAME");
+                        //controller.initiateGame()
+                    } else {
+                        //fortsette loadingscreen?
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         // TODO: dersom det er to spillere i "availablePlayer" -> kall
-        // controller.initateGame()
-
     }
 
 }
