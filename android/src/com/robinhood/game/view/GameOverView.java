274559180492c947f216cc298a956fe58d1eedbb
Charlotte Maigull Söderström <cmrisbak@stud.ntnu.no>
2020-04-16 18:01:14 +0200

    Added scene 2d base to GameOverView

--- android/src/com/robinhood/game/view/GameOverView.java
+++ android/src/com/robinhood/game/view/GameOverView.java
@@ -2,11 +2,17 @@ package com.robinhood.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.robinhood.game.controller.Controller;
 import com.robinhood.game.model.Model;
@@ -23,12 +29,52 @@ public class GameOverView extends View {
     private BitmapFont font = new BitmapFont();
     private int playerNrWinner;
 
+    // Creating text field for game results
+    Skin skin = new Skin(Gdx.files.internal("skin/dark-hdpi/Holo-dark-hdpi.json"));
+    TextButton gameOver = new TextButton("Game Over", skin);
+    TextButton gameResult = new TextButton("Player 1 won the game", skin);
+    TextButton menu = new TextButton("Menu", skin);
+
     public GameOverView(Controller cont, Model model) {
         this.controller = cont;
 
         super.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(super.stage);
 
+
+        // Create new table that fills the screen -> Table added to stage
+        Table table = new Table();
+        table.setFillParent(true);
+        // Cheap way to add background, fix later using asset manager
+        table.setBackground(
+                new TextureRegionDrawable(
+                        new TextureRegion(
+                                new Texture("background-smaller.png")
+                        )
+                )
+        );
+        // table.setPosition(400, 0);      // Want to move the buttons to the right
+        stage.addActor(table);
+
+        table.row().pad(400, 0, 10, 0);
+        table.add(gameOver).fillX().uniform().width(300f).height(100f);
+        table.row().pad(10, 0, 10, 0);
+        table.add(gameResult).fillX().uniform().width(300f).height(100f);
+        table.row().pad(10, 0, 10, 0);
+        table.add(menu).fillX().uniform().width(300f).height(100f);
+        table.row().pad(10, 0, 10, 0);
+
+        // ClickListener triggered by user clicks on Button/Actor to call appropriate actions
+        menu.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float clickX, float clickY) {
+                controller.cancelFindPlayer();
+                controller.navigateTo("MENU");
+            }
+        });
+
+
+/*
         Button menuButton = new Button("menu");
         menuButton.addListener(new ClickListener() {
             @Override
@@ -38,6 +84,8 @@ public class GameOverView extends View {
             }
         });
         stage.addActor(menuButton);
+
+ */
 
         List<Integer> hpList = controller.getHP();
         playerNrWinner = 0;
