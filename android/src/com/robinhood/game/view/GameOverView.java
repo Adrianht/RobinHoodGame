367379195295d929cf31511bef2b98d43bcb28b6
Ola Hermann Opheim <olahermannopheim@Olas-MacBook-Pro.local>
2020-04-18 16:57:53 +0200

    swapped playerX to username of winner. deleted unused lines.

--- android/src/com/robinhood/game/view/GameOverView.java
+++ android/src/com/robinhood/game/view/GameOverView.java
@@ -1,10 +1,7 @@
 package com.robinhood.game.view;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -18,32 +15,26 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.robinhood.game.controller.Controller;
 import com.robinhood.game.model.Model;
-import com.robinhood.game.view.interfaceObjects.Button;
-
-import java.util.List;
 
 
 public class GameOverView extends View {
 
     private final Controller controller;
 
-    //private SpriteBatch batch = new SpriteBatch();
-    //private BitmapFont font = new BitmapFont();
-    private int playerNrWinner;
-
-    // Creating text field for game results
-    Skin skinButton = new Skin(Gdx.files.internal("skin/dark-hdpi/Holo-dark-hdpi.json"));
-    Skin skinGameOver = new Skin(Gdx.files.internal("skin/craftacular/craftacular-ui.json"));
-    Skin skinResult = new Skin(Gdx.files.internal("skin/shade/uiskin.json"));
-    Label gameOver = new Label("GAME OVER", skinGameOver);
-    Label gameResult = new Label("Player " + playerNrWinner + " won the game!", skinResult);
-    TextButton menu = new TextButton("Menu", skinButton);
-
     public GameOverView(Controller cont, Model model) {
         this.controller = cont;
+        String usernameWinner = controller.getWinner();
 
         super.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(super.stage);
+
+        // Creating text field for game results
+        Skin skinButton = new Skin(Gdx.files.internal("skin/dark-hdpi/Holo-dark-hdpi.json"));
+        Skin skinGameOver = new Skin(Gdx.files.internal("skin/craftacular/craftacular-ui.json"));
+        Skin skinResult = new Skin(Gdx.files.internal("skin/shade/uiskin.json"));
+        Label gameOver = new Label("GAME OVER", skinGameOver);
+        Label gameResult = new Label(usernameWinner + " won the game!", skinResult);
+        TextButton menu = new TextButton("Menu", skinButton);
         
         // Create new table that fills the screen -> Table added to stage
         Table table = new Table();
@@ -80,32 +71,6 @@ public class GameOverView extends View {
             }
         });
 
-        List<Integer> hpList = controller.getHP();
-        playerNrWinner = 0;
-        for (int i = 0; i < hpList.size(); i++ ){
-            if(hpList.get(i) > 0) {
-                playerNrWinner = i;
-                break;
-            }
-        }
-
         controller.endGameInstance();
     }
-
-    /*
-    @Override
-    public void render() {
-        float[] values = hextoRGB("#5f8db0");
-        Gdx.gl.glClearColor(values[0], values[1], values[2], 1.0f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.draw();
-        batch.begin();
-        font.draw(batch,
-                ("GAME OVER..." +
-                    "\ṅPlayer number " + playerNrWinner + " won the game."),
-                250, 250);
-        batch.end();
-    }
-
-     */
 }
