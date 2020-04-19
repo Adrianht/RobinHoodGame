231b427342758d245d1b8ab8a21ce3374f2204cf
Ola Hermann Opheim <olahermannopheim@Olas-MacBook-Pro.local>
2020-04-19 10:15:34 +0200

    add arrow shape

--- android/src/com/robinhood/game/model/EntityFactory.java
+++ android/src/com/robinhood/game/model/EntityFactory.java
@@ -1,6 +1,5 @@
 package com.robinhood.game.model;
 
-import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.ChainShape;
@@ -75,44 +74,21 @@ public class EntityFactory {
         entity.addComponent("arrowType");
         entity.addComponent("box2d");
         entity.component.box2dBody.body = world.createBody(arrowBodyDef);
-
         ChainShape chainShape = new ChainShape();
-        //polygonShape.setAsBox(.5f,.2f);
-        Vector2[] verticies = new Vector2[14];
-        verticies[0] = new Vector2(.3f,0f);
-
-        verticies[1] = new Vector2(.4f,.1f);
-
-        //dummy
-        verticies[2] = new Vector2(.08f,.1f);
-
-        verticies[3] = new Vector2(1.3f,.1f);
-
-        verticies[4] = new Vector2(1.3f,.05f);
-
-        verticies[5] = new Vector2(1.5f,.15f);
-
-        verticies[6] = new Vector2(1.3f,.25f);
-
-        verticies[7] = new Vector2(1.3f,.2f);
-
-        //dummy
-        verticies[8] = new Vector2(.08f,.2f);
-
-        verticies[9] = new Vector2(.4f,.2f);
-
-        verticies[10] = new Vector2(.3f,.3f);
-
-        verticies[11] = new Vector2(0f,.3f);
-        verticies[12] = new Vector2(.1f,.15f);
-        verticies[13] = new Vector2(0f,0f);
-
-
-        chainShape.createLoop(verticies);
+        float[] arrowShapeCoordinatesX = {.3f, .4f, .08f, 1.3f,
+                1.3f, 1.5f, 1.3f, 1.3f, .08f, .4f, .3f, 0f, .1f, 0f};
+        float[] arrowShapeCoordinatesY = {0f, .1f, .1f, .1f,
+                .05f, .15f, .25f, .2f, .2f, .2f, .3f, .3f, .15f, 0f};
+        Vector2[] vertices = new Vector2[arrowShapeCoordinatesX.length];
+        for (int i=0; i<arrowShapeCoordinatesX.length; i++) {
+            vertices[i] = new Vector2(
+                    arrowShapeCoordinatesX[i],
+                    arrowShapeCoordinatesY[i]
+            );
+        }
+        chainShape.createLoop(vertices);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = chainShape;
-
-
         fixtureDef.density = 0.5f;
         fixtureDef.friction = 0.7f;
         fixtureDef.restitution = 0.3f;
