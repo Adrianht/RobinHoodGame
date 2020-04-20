139c15a25ff17edf036db268588affada0c2910b
Charlotte Maigull Söderström <cmrisbak@stud.ntnu.no>
2020-04-20 16:59:26 +0200

    updated the instructions and repositioned the table

--- android/src/com/robinhood/game/view/InstructionsView.java
+++ android/src/com/robinhood/game/view/InstructionsView.java
@@ -20,12 +20,6 @@ public class InstructionsView extends View {
     public InstructionsView(Controller controller) {
         super(controller);
 
-        // Create and position text fields and buttons of current UI
-        /* Skin skinHeading = new Skin(Gdx.files.internal(
-                "skin/craftacular/craftacular-ui.json"));
-
-         */
-
         Skin skinInstruction = new Skin(Gdx.files.internal(
                 "skin/shade/uiskin.json"));
 
@@ -38,43 +32,41 @@ public class InstructionsView extends View {
                         "- Hitting the opponent results in the opponent losing points\n"
                 , skinInstruction);
         Label instructions2 = new Label(
-                " - Every move costs 10 energy points\n" +
+                " - Every move costs 2 energy points\n" +
                         "- Upgrade your weapon if wanted before making the shot\n" +
                         "- Drag your finger and aim towards the opponent\n" +
                         "- When aimed, let go of the screen to shoot the opponent\n"
                 , skinInstruction);
         Label instructions3 = new Label(
-                " - The players both start with 100(?) HitPoints and 20(?) EnergyPoints. \n" +
+                " - Both players start with 100 HitPoints and 20 EnergyPoints. \n" +
                         "- EnergyPoints can be used to purchase upgraded arrows with increased damage\n" +
                         "- The cost of the upgraded arrows are indicated in the game \n" +
-                        "- For every turn, the players gains 20(?) EnergyPoints \n"
+                        "- For every turn, the players gains 10 EnergyPoints \n"
                 , skinInstruction);
         TextButton menu = new TextButton("Menu", skinButton);
 
-        table.row().pad(0, 0, 10, 0);
-        //table.add(instructionsHeading).fillX().uniform().width(300f).height(100f);
-        table.row().pad(0, 0, 10, 0);
-        //instructionsHeading.setAlignment(Align.center); // Align center
+        table.bottom();
+        table.row().pad(20f, 0, 30f, 0);
+        table.row().pad(0, 0, 30f, 0);
 
         table.add(instructions1).fillX().uniform().width(300f).height(100f);
-        table.row().pad(30, 0, 30, 0);
-        instructions1.setAlignment(Align.center); // Align center
+        table.row().pad(30f, 0, 30f, 0);
+        instructions1.setAlignment(Align.center);
 
         table.add(instructions2).fillX().uniform().width(300f).height(100f);
-        table.row().pad(30, 0, 30, 0);
-        instructions2.setAlignment(Align.center); // Align center
+        table.row().pad(30f, 0, 50f, 0);
+        instructions2.setAlignment(Align.center);
 
         table.add(instructions3).fillX().uniform().width(300f).height(100f);
-        table.row().pad(30, 0, 30, 0);
+        table.row().pad(30f, 0, 80f, 0);
         instructions3.setAlignment(Align.center); // Align center
 
         table.add(menu).fillX().uniform().width(300f).height(100f);
-        table.row().pad(30, 0, 0, 0);
+        table.row().pad(10f, 0, 60f, 0);
 
-        //instructionsHeading.setFontScale(4.0f );
-        instructions1.setFontScale(2.0f);
-        instructions2.setFontScale(2.0f);
-        instructions3.setFontScale(2.0f);
+        instructions1.setFontScale(2.5f);
+        instructions2.setFontScale(2.5f);
+        instructions3.setFontScale(2.5f);
 
         menu.addListener(generateNavigateListener("MENU"));
     }
